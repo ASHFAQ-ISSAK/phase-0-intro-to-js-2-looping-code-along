19c9b9496ca748d2ea3000acb1420840c8390639
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-19 10:39:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  const messages = [];
+
+  for (const name of names) {
+    const message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
